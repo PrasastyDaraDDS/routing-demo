723072745225ede46e3b1aeda80fa098e731d796
PrasastyDaraDSS <daraprasasty075@gmail.com>
2024-03-15 21:26:24 +0700

    praktikum5

---
+++ src/app/blogs/second/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SecondBlog: React.FC = () => {
+    return (
+        <div>
+            <h1>Second Blog</h1>
+        </div>
+    );
+};
+
+export default SecondBlog;
---
+++ src/app/profile/page.tsx
@@ -0,0 +1,18 @@
+// pages/profile.js
+
+import React from 'react';
+
+const ProfilePage = () => {
+  return (
+    <div>
+      <h1>My Profile</h1>
+      <p>Name: Prasasty Dara DS</p>
+      <p>Age: 20</p>
+      <p>Location: Malang</p>
+      <p>University : Politeknik Negeri Malang</p>
+      <p>Hobi : reading</p>
+    </div>
+  );
+};
+
+export default ProfilePage;
